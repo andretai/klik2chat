a39161adffb0bbfd89a713dff006eb3d96ae2139
Andre Tai <andretai.wx@gmail.com>
2021-10-30 12:31:00 +0800

    removed add to home screen button functionality (for now)

--- src/App.js
+++ src/App.js
@@ -24,35 +24,6 @@ const classes = {
 
 function App() {
 
-  let deferredPrompt;
-  const addBtn = document.querySelector('.add_button');
-  addBtn.style.display = 'none';
-
-  window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen
-    addBtn.style.display = 'block';
-
-    addBtn.addEventListener('click', () => {
-      // hide our user interface that shows our A2HS button
-      addBtn.style.display = 'none';
-      // Show the prompt
-      deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-    });
-  });
-
   // states
 
   const [drawer, toggleDrawer] = React.useState(false)
@@ -122,9 +93,6 @@ function App() {
 
   return (
     <Container maxWidth="sm">
-      <Button variant="contained" className={classes.add_button}>
-        Add to Home Screen
-      </Button>
       <Header 
         login={login} 
         logout={logout} 
