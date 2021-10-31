b13286d51d3493f2b25ee9309d80a8c8698a5be7
Andre Tai <andretai.wx@gmail.com>
2021-11-01 00:43:35 +0800

    test ad placement 1

--- src/components/Ads.js
+++ src/components/Ads.js
@@ -2,16 +2,13 @@ import React from 'react'
 
 const Ads = () => {
 
-  React.useEffect(() => {
-    (window.adsbygoogle = window.adsbygoogle || []).push({});
-  }, [])
-
   return (
-    <ins className='adsbygoogle'
-      style={{ display: 'block' }}
-      data-ad-client='pub-9285094871779032'
-      data-ad-slot='8471250112'
-      data-ad-format='auto' />
+    <ins className="adsbygoogle"
+     style={{ display: 'block' }}
+     data-ad-client="ca-pub-9285094871779032"
+     data-ad-slot="2391617283"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
   )
 }
 
